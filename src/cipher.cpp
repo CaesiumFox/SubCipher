ccf1696b626e09047cb09f5dca4b6c72479e22ed
CaesiumFox <avdeev20020904@gmail.com>
2025-08-03 17:04:04 +0700

    Remove dummy

--- src/cipher.cpp
+++
@@ -1,7 +0,0 @@
-#include "cipher.hpp"
-
-#include <iostream>
-
-void sample_function() {
-    std::cout << "Sample function" << std::endl;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
-#include "cipher.hpp"
-
 #include "cxxopts.hpp"
 
 int main(int argc, char** argv) {
-    sample_function();
     return 0;
 }
